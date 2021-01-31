12bac326350023a6216b9d401e35125bd1cf7fe4
nikolaeff80 <nikolaeff2007@gmail.com>
2021-01-31 15:19:53 +0300

    Changes according to PEP 8

--- setup_WR.py
+++ setup_WR.py
@@ -2,14 +2,14 @@ import sys
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
-    # "packages": ["main"],
+    # 'packages': ['main'],
 }
 setup(
-    name="Weather_Reminder",
-    version="0.0.1",
-    description="Weather Reminder for Jove",
+    name='Weather_Reminder',
+    version='0.0.1',
+    description='Weather Reminder for Jove',
     options={
-        "build_exe": build_exe_options
+        'build_exe': build_exe_options
     },
     executables=[Executable('Weather_Reminder.py',
                             base='Win32GUI',
